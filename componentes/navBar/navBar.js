a4bb2d4982e03790adced8046a5299cd056b97f6
Santiago <slopez@scl.edu.gt>
2024-09-24 12:03:11 -0600

    actualizacion de esta rama

--- componentes/navBar/navBar.js
+++ componentes/navBar/navBar.js
@@ -1,16 +1,17 @@
-let navBarDiv = document.createElement(`div`);
-navBarDiv.className = "nav-bart-Div";
+let navBarDiv = document.createElement('div');
+navBarDiv.className = "nav-bar-div";
 
-let imgLogo = document.createElement(`img`)
-imgLogo.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1200px-Tux.svg.png";
-imgLogo.localName = "img-log"
+let imgLogo = document.createElement('img')
+imgLogo.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5_10kNUjHNXi_8OiEsEjUllXdK2lt5hL9pQ&s";
+imgLogo.className = "img-log"
 navBarDiv.appendChild(imgLogo);
 
-let h2 = document.createElement(`h2`);
+
+let h2 = document.createElement('h2');
 h2.innerText = "Santiago Lopez";
 navBarDiv.appendChild(h2);
 
-export {navBarDiv}
+export {navBarDiv};
 
 
 
--- main.js
+++ main.js
@@ -3,13 +3,13 @@ import { navBarDiv } from "./componentes/navBar/navBar.js";
 let DOM = document.querySelector("#root");
 
 /*navBar */
-let navBar = document.createElement(`section`);
-navBar.className = "nav-Bar";
+let navBar = document.createElement('section');
+navBar.className = "nav-bar";
 navBar.appendChild = (navBarDiv);
 DOM.appendChild(navBar);
 
-let proyect = document.createElement(`section`);
-proyect.classList = "proyect";
+let proyect = document.createElement('section');
+proyect.className = "proyect";
 proyect.innerText = "Hola mundo soy proyect";
 DOM.appendChild(proyect);
 
