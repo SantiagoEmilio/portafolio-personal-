149279ee07aaaf57f2a8129da480a3d423f53d7f
Santiago <slopez@scl.edu.gt>
2024-09-24 12:56:26 -0600

    actualizacion

--- componentes/navBar/navBar.js
+++ componentes/navBar/navBar.js
@@ -1,17 +1,13 @@
 let navBarDiv = document.createElement('div');
 navBarDiv.className = "nav-bar-div";
 
-let imgLogo = document.createElement('img')
+let imgLogo = document.createElement('img');
 imgLogo.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5_10kNUjHNXi_8OiEsEjUllXdK2lt5hL9pQ&s";
-imgLogo.className = "img-log"
+imgLogo.className = "img-logo";
 navBarDiv.appendChild(imgLogo);
 
-
 let h2 = document.createElement('h2');
-h2.innerText = "Santiago Lopez";
+h2.innerText = "Santiago";
 navBarDiv.appendChild(h2);
 
-export {navBarDiv};
-
-
-
+export { navBarDiv };
