fdb264759d6416a841e288cdc5e2a32c93f51921
Santiago <slopez@scl.edu.gt>
2024-09-26 10:13:07 -0600

    segundo vercion del portafolio

--- componentes/PROYECTOS /proyectos.js
+++ componentes/PROYECTOS /proyectos.js
@@ -1,3 +1,5 @@
+import { header } from "../header/header.js"
+
 function proyectos(){
     let section = document.createElement('section');
     section.appendChild(header())
@@ -10,21 +12,7 @@ function proyectos(){
 }
 
 
-function header(){
-   let div = document.createElement('div');
-   div.className = "header";
 
-   let h2 = document.createElement('h1');
-   h2.className = "titulo";
-   h2.innerText = "proyectos";
-   div.appendChild(h2);
-
-   let btn = document.createElement('div');
-   btn.innerText = "git hub"
-   div.appendChild(btn);
-
-return div;
-}
 
 
 function item(){
