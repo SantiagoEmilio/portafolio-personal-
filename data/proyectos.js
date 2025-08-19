04451ec33952f3b5cb3ba5f95b525b0419df1577
Anonimo <anonimo@example.com>
2025-08-19 11:50:26 -0600

    se actualiza mi portafolio personal

---
+++ data/proyectos.js
@@ -0,0 +1,37 @@
+// Aquí defines tus proyectos y stacks de forma dinámica
+
+const stacks = [
+  { nombre: "HTML", icon: "assets/stacks/html.png" },
+  { nombre: "CSS", icon: "assets/stacks/css.png" },
+  { nombre: "JavaScript", icon: "assets/stacks/js.png" },
+  { nombre: "Java", icon: "assets/stacks/java.jpg" },
+  { nombre: "Python", icon: "assets/stacks/python.png" },
+];
+
+const proyectos = [
+  {
+    nombre: "frontEnd-asistencia",
+    stacks: ["HTML", "CSS", "sql","JavaScript"],
+    page: "https://rockemma.github.io/frontEnd-asistencia/",
+    repo: "https://github.com/ROCKEMMA/frontEnd-asistencia"
+  },
+  {
+    nombre: "Proyectos--Formularios--santi",
+    stacks: ["HTML", "CSS", "JavaScript"],
+    page: "https://santiagoemilio.github.io/Proyectos--Formularios--santi/",
+    repo: "https://github.com/SantiagoEmilio/Proyectos--Formularios--santi"
+  },
+  {
+    nombre: "ExamenP2",
+    stacks: ["HTML", "CSS", "JavaScript"],
+    page: "https://santiagoemilio.github.io/ExamenP2/",
+    repo: "https://github.com/SantiagoEmilio/ExamenP2"
+  },
+
+  {
+    nombre: "Galeria de productos ",
+    stacks: ["HTML", "CSS", "JavaScript"],
+    page: "https://santiagoemilio.github.io/galeriaProductos-/",   
+    repo: "https://github.com/SantiagoEmilio/galeriaProductos-"
+  }
+];
